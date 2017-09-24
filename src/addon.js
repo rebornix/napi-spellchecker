c2e43ebe55b6c30e7faa12a559e28a43401bc26e
rebornix <penn.lv@gmail.com>
2017-09-24 15:29:30 -0700

    update initializer and tests

--- src/addon.js
+++ src/addon.js
@@ -1,12 +1,7 @@
 var addon = require('bindings')('spellchecker');
 
-var obj = new addon.Spellchecker(10);
+var obj = new addon.Spellchecker();
 
-console.log( obj.isMisspelled("helloo") );
-console.log( obj.isMisspelled("hello") );
-console.log( obj.getCorrectionsForMisspelling("helloo").join(', '));
-var misRanges = obj.checkSpelling("helloo wrold");
-for (var i = 0; i < misRanges.length; i++) {
-    console.log(`start: ${misRanges[i].start}, end: ${misRanges[i].end}`);
-}
-console.log( obj.getAvailableDictionaries().join(', ') );
+console.log( obj.add("cliché") );
+console.log( obj.checkSpelling() );
+
--- src/spellcheckWrap.cc
+++ src/spellcheckWrap.cc
@@ -48,11 +48,17 @@ std::string Spellchecker::ParseWord(napi_env env, napi_value str)
 
     size_t strLength = 1;
     status = napi_get_value_string_utf8(env, str, NULL, 0, &strLength);
-    assert(status == napi_ok);
+    if (status != napi_ok) {
+        napi_throw_error(env, nullptr, "Bad argument");
+        return nullptr;
+    }
     char buf[strLength];
     size_t result;
     status = napi_get_value_string_utf8(env, str, buf, strLength + 1, &result);
-    assert(status == napi_ok);
+    if (status != napi_ok) {
+        napi_throw_error(env, nullptr, "Bad argument");
+        return nullptr;
+    }
 
     std::string word(buf);
     return word;
@@ -83,26 +89,12 @@ napi_value Spellchecker::New(napi_env env, napi_callback_info info)
 
     if (is_constructor)
     {
-        // Invoked as constructor: `new Spellchecker(...)`
-        size_t argc = 1;
-        napi_value args[1];
+        // Invoked as constructor: `new Spellchecker()`
         napi_value jsthis;
-        status = napi_get_cb_info(env, info, &argc, args, &jsthis, nullptr);
+        status = napi_get_cb_info(env, info, nullptr, nullptr, &jsthis, nullptr);
         assert(status == napi_ok);
 
-        double value = 0;
-
-        napi_valuetype valuetype;
-        status = napi_typeof(env, args[0], &valuetype);
-        assert(status == napi_ok);
-
-        if (valuetype != napi_undefined)
-        {
-            status = napi_get_value_double(env, args[0], &value);
-            assert(status == napi_ok);
-        }
-
-        Spellchecker *obj = new Spellchecker(value);
+        Spellchecker *obj = new Spellchecker();
         obj->spellcheckerImpl_ = SpellcheckerFactory::CreateSpellchecker();
 
         obj->env_ = env;
@@ -118,21 +110,13 @@ napi_value Spellchecker::New(napi_env env, napi_callback_info info)
     }
     else
     {
-        // Invoked as plain function `Spellchecker(...)`, turn into construct call.
-        size_t argc_ = 1;
-        napi_value args[1];
-        status = napi_get_cb_info(env, info, &argc_, args, nullptr, nullptr);
-        assert(status == napi_ok);
-
-        const size_t argc = 1;
-        napi_value argv[argc] = {args[0]};
-
+        // Invoked as plain function `Spellchecker()`, turn into construct call.
         napi_value cons;
         status = napi_get_reference_value(env, constructor, &cons);
         assert(status == napi_ok);
 
         napi_value instance;
-        status = napi_new_instance(env, cons, argc, argv, &instance);
+        status = napi_new_instance(env, cons, 0, nullptr, &instance);
         assert(status == napi_ok);
 
         return instance;
@@ -247,15 +231,25 @@ napi_value Spellchecker::CheckSpelling(napi_env env, napi_callback_info info)
     napi_value args[1];
     napi_value jsthis;
     status = napi_get_cb_info(env, info, &argc, args, &jsthis, nullptr);
-    assert(status == napi_ok);
+    if (status != napi_ok || argc < 1) {
+        napi_throw_error(env, nullptr, "Bad argument");
+        return nullptr;
+    }
 
     size_t strLength = 1;
     status = napi_get_value_string_utf8(env, args[0], NULL, 0, &strLength);
-    assert(status == napi_ok);
+    if (status != napi_ok || argc < 1) {
+        napi_throw_error(env, nullptr, "Bad argument");
+        return nullptr;
+    }
+
     char16_t buf[strLength];
     size_t result;
     status = napi_get_value_string_utf16(env, args[0], buf, strLength + 1, &result);
-    assert(status == napi_ok);
+    if (status != napi_ok) {
+        napi_throw_error(env, nullptr, "Bad argument");
+        return nullptr;
+    }
 
     Spellchecker *obj = Spellchecker::Unwrap(env, info);
 
@@ -298,12 +292,16 @@ napi_value Spellchecker::Add(napi_env env, napi_callback_info info)
     size_t argc = 1;
     napi_value args[1];
     status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
-    assert(status == napi_ok);
+    if (status != napi_ok || argc < 1) {
+        napi_throw_error(env, nullptr, "Bad argument");
+        return nullptr;
+    }
 
     std::string word = Spellchecker::ParseWord(env, args[0]);
     Spellchecker *obj = Spellchecker::Unwrap(env, info);
 
     obj->spellcheckerImpl_->Add(word);
+    return nullptr;
 }
 
 napi_value Spellchecker::Remove(napi_env env, napi_callback_info info)
@@ -312,10 +310,14 @@ napi_value Spellchecker::Remove(napi_env env, napi_callback_info info)
     size_t argc = 1;
     napi_value args[1];
     status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
-    assert(status == napi_ok);
+    if (status != napi_ok || argc < 1) {
+        napi_throw_error(env, nullptr, "Bad argument");
+        return nullptr;
+    }
 
     std::string word = Spellchecker::ParseWord(env, args[0]);
     Spellchecker *obj = Spellchecker::Unwrap(env, info);
 
     obj->spellcheckerImpl_->Remove(word);
+    return nullptr;
 }
